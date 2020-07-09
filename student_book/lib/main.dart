1124f4c67029ab3ea37e9e508b2971ea606e08dd
ItsJuniorDias <juniordias_@live.com>
2020-07-08 22:29:38 -0300

    Login and signUp

--- student_book/lib/main.dart
+++ student_book/lib/main.dart
@@ -1,28 +1,39 @@
+import 'package:compound/ui/views/signup_view.dart';
 import 'package:flutter/material.dart';
-import 'package:student_book/pages/Home.dart';
-import 'package:student_book/pages/SignUp.dart';
-import 'package:student_book/pages/Login.dart';
+import 'package:compound/services/navigation_service.dart';
+import 'package:compound/services/dialog_service.dart';
+import 'package:compound/ui/views/login_view.dart';
+import 'managers/dialog_manager.dart';
+import 'ui/router.dart';
+import 'locator.dart';
 
 void main() {
+  // Register all the models and services before the app starts
+  setupLocator();
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'E-commerce',
-        theme: ThemeData(
-            fontFamily: 'Metropolis',
-            primarySwatch: Colors.blue,
-            backgroundColor: Color(0xFFE5E5E5)),
-        //home: SignIn(),
-        initialRoute: '/lib/pages/Login',
-        routes: {
-          "/lib/pages/Login": (context) => SignIn(),
-          "/lib/pages/SignUp": (context) => SignUp(),
-          "/lib/pages/Home": (context) => Home(),
-        });
+      title: 'Compound',
+      builder: (context, child) => Navigator(
+        key: locator<DialogService>().dialogNavigationKey,
+        onGenerateRoute: (settings) => MaterialPageRoute(
+            builder: (context) => DialogManager(child: child)),
+      ),
+      navigatorKey: locator<NavigationService>().navigationKey,
+      theme: ThemeData(
+        primaryColor: Color.fromARGB(255, 9, 202, 172),
+        backgroundColor: Color.fromARGB(255, 26, 27, 30),
+        textTheme: Theme.of(context).textTheme.apply(
+              fontFamily: 'Open Sans',
+            ),
+      ),
+      home: LoginView(),
+      onGenerateRoute: generateRoute,
+    );
   }
 }
---
+++ student_book/lib/ui/views/signup_view.dart
@@ -0,0 +1,65 @@
+import 'package:compound/ui/shared/ui_helpers.dart';
+import 'package:compound/ui/widgets/busy_button.dart';
+import 'package:compound/ui/widgets/input_field.dart';
+import 'package:flutter/material.dart';
+import 'package:provider_architecture/provider_architecture.dart';
+import 'package:compound/viewmodels/signup_view_model.dart';
+
+class SignUpView extends StatelessWidget {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelProvider<SignUpViewModel>.withConsumer(
+      viewModel: SignUpViewModel(),
+      builder: (context, model, child) => Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 50.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Registre-se',
+                style: TextStyle(
+                  fontSize: 38,
+                ),
+              ),
+              verticalSpaceLarge,
+              // TODO: Add additional user data here to save (episode 2)
+              InputField(
+                placeholder: 'Email',
+                controller: emailController,
+              ),
+              verticalSpaceSmall,
+              InputField(
+                placeholder: 'Senha',
+                password: true,
+                controller: passwordController,
+                additionalNote: 'A senha deve ter no mínimo 6 caracteres.',
+              ),
+              verticalSpaceMedium,
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  BusyButton(
+                    title: 'Entrar',
+                    busy: model.busy,
+                    onPressed: () {
+                      model.signUp(
+                          email: emailController.text,
+                          password: passwordController.text);
+                    },
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
